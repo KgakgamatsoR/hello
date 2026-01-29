b5b22ce7e7e2894d559968b5798eb3f70a107763
kgakgamatso_r <katlego.ramatlo2@gmail.com>
2026-01-29 10:16:21 +0200

    some lines are missing

--- hello.py
+++ hello.py
@@ -2,5 +2,5 @@ print("Lecture 1 of learning git")
 
 print("Hello world")
 print("Here are somechanges to track")
-print("I like the little community I am building for myself")
-
+print("I like the little community I am building for myself!")
+print("lerning how to code in python is fun.")
