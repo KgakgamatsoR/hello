8d0797323743ed939bff00b9da5cb22b058328d6
kgakgamatso_r <katlego.ramatlo2@gmail.com>
2026-01-29 10:13:37 +0200

    fixed typos

--- hello.py
+++ hello.py
@@ -2,4 +2,5 @@ print("Lecture 1 of learning git")
 
 print("Hello world")
 print("Here are somechanges to track")
-print("I like the little community I am buuilding for myslef")
+print("I like the little community I am building for myself")
+
