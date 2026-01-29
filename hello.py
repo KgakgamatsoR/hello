2256f9deae256179c5ad4ee1cf0bdb6106fa8995
kgakgamatso_r <katlego.ramatlo2@gmail.com>
2026-01-30 00:01:12 +0200

    Added new line on test_branch

--- hello.py
+++ hello.py
@@ -2,3 +2,5 @@ print("Lecture 1 - Learning git")
 
 print("lerning how to code in python is fun.")
 print("I like the little community I am building for myself!")
+
+print("I just added a new sntence to a new branch!")
