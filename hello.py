e87ae7239ca8adb2389a17b40b809196e3aa5e39
kgakgamatso_r <katlego.ramatlo2@gmail.com>
2026-01-30 13:08:34 +0200

    fix typos

--- hello.py
+++ hello.py
@@ -3,4 +3,4 @@ print("Lecture 1 - Learning git")
 print("lerning how to code in python is fun.")
 print("I like the little community I am building for myself!")
 
-print("I just added a new sntence to a new branch!")
+print("I just added a new sentence to a new branch!")
